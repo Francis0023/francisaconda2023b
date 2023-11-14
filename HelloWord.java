711bb70977867739418b5676df56dbba9e41f664
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2023-11-14 09:13:40 -0500

    Version 3

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,9 @@
 public class HelloWord{
   public static void main(String [] args){
-    System.out.println("Analisis de datos"); 
+    System.out.println("***** Suma *****0");
+    int num1=2,num2=8;
+    float resul;
+    resul=num1+num2;
+    System.out.println("La suma es: "+resul);
   }
 }
